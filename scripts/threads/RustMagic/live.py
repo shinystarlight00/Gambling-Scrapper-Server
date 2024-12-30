16f2c9631df3a57ab965da1f8009919ec4504684
shinystarlight00 <charles.dev.0811@gmail.com>
2024-12-30 19:38:28 +0000

    fix: uncomment headless mode

--- scripts/threads/RustMagic/live.py
+++ scripts/threads/RustMagic/live.py
@@ -21,7 +21,7 @@ class RustMagic:
         ua = UserAgent()
 
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument("--incognito")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("window-size=1920,1080")
